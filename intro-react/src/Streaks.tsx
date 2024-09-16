6f730a0ee0500146420ad281b17115b68ef689e8
D3S002 <abdulhma@hiof.no>
2024-09-16 17:12:51 +0200

    React

---
+++ intro-react/src/Streaks.tsx
@@ -0,0 +1,10 @@
+export default function Streaks() {
+    const streaks = 10;
+
+    return (
+        <main>
+            <h1>This is your streaks: {streaks}</h1>
+            <p>This is the secound page</p>
+        </main>
+    );
+}
