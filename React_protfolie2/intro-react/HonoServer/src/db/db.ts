585fe0ccb2904ac15c04d48383027a95fbe11e4f
D3S002 <abdulhma@hiof.no>
2024-10-28 16:35:35 +0100

    database

--- React_protfolie2/intro-react/HonoServer/src/db/db.ts
+++ React_protfolie2/intro-react/HonoServer/src/db/db.ts
@@ -1,8 +1,17 @@
 import { env } from "../lib/env";
 import { makeLogger } from "../lib/logger";
 import Database from "better-sqlite3";
+import fs from "node:fs";
+import path from "node:path";
 
-export const db = new Database(env.DATABASE_URL, {
+const dbFilePath = env.DATABASE_URL.replace(/^file:/, "");
+const dbPath = path.dirname(dbFilePath);
+
+if (!fs.existsSync(dbPath)) {
+  fs.mkdirSync(dbPath, { recursive: true });
+}
+
+export const db = new Database(dbFilePath, {
   verbose: (message: unknown) => makeLogger().info(`${message}`),
 });
 
